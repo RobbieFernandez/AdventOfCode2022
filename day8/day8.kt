9d3422c6633591c354a5fed8ee305ebfeab5ebea
Robert Fernandez <fernandez.robert.97@gmail.com>
2022-12-08 16:09:25 +0800

    Day 8 rewrite

--- day8/day8.kt
+++ day8/day8.kt
@@ -1,79 +1,81 @@
 import java.io.File
 
-fun buildGrid(input: Iterable<String>): List<List<Int>> = input.map { line ->
-    line.map { Integer.parseInt(it.toString()) }
-}
+class Grid(input: List<String>) {
+    private val grid: List<List<Int>>
 
-fun isVisible(grid: List<List<Int>>, cell: Pair<Int, Int>): Boolean {
-    val cellValue = grid[cell.first][cell.second]
-    val row = grid[cell.first]
-    val visibleFromLeft = row.slice(0 until cell.second).all {
-        it < cellValue
+    init {
+        grid = input.map { line ->
+            line.map { Integer.parseInt(it.toString()) }
+        }
     }
-    if (visibleFromLeft) return true
 
-    val visibleFromRight = row.slice(cell.second + 1 until row.size).all {
-        it < cellValue
+    val width: Int get() = grid[0].size
+    val height: Int get() = grid.size
+
+    class Tree(private val grid: Grid, private val col: Int, private val row: Int, val height: Int) {
+        private fun left() = if (col > 0) grid.getTree(row, col - 1) else null
+        private fun right() = if (col < (grid.width - 1)) grid.getTree(row, col + 1) else null
+        private fun up() = if (row > 0) grid.getTree(row - 1, col) else null
+        private fun down() = if (row < (grid.height - 1)) grid.getTree(row + 1, col) else null
+
+        private fun walk(nextTree: (Tree) -> Tree?): Sequence<Tree> {
+            val thisTree = this;
+            return sequence<Tree> {
+                var tree = nextTree(thisTree)
+                while (tree !== null) {
+                    yield(tree)
+                    tree = nextTree(tree)
+                }
+            }
+        }
+
+        private fun firstBlockingTree(nextTree: (Tree) -> Tree?): Tree? =
+            walk(nextTree).find { it.height >= height }
+
+        private fun walkLeft() = walk { it.left() }
+        private fun walkRight() = walk { it.right() }
+        private fun walkUp() = walk { it.up() }
+        private fun walkDown() = walk { it.down() }
+
+        private fun visibilityLeft() = firstBlockingTree { it.left() }?.let{ col - it.col } ?: col
+        private fun visibilityRight() = firstBlockingTree { it.right() }?.let { it.col - col } ?: (grid.width - col - 1)
+        private fun visibilityUp() = firstBlockingTree { it.up() }?.let { row - it.row } ?: row
+        private fun visibilityDown() = firstBlockingTree { it.down() }?.let { it.row - row } ?: (grid.height - row - 1)
+
+        fun isVisible()= (
+            walkLeft().all { it.height < height} ||
+            walkRight().all { it.height < height} ||
+            walkUp().all { it.height < height} ||
+            walkDown().all { it.height < height}
+        )
+
+        fun scenicScore()= listOf(
+            visibilityLeft(),
+            visibilityRight(),
+            visibilityUp(),
+            visibilityDown()
+        ).reduce { acc, it -> acc * it}
     }
-    if (visibleFromRight) return true
 
-    val col = grid.map { it[cell.second] }
-    val visibleFromTop = col.slice(0 until cell.first).all {
-        it < cellValue
+    fun getTree(row: Int, col: Int) = Tree(this, col, row, grid[row][col])
+
+    fun treeIterator() = sequence {
+        for (row in (0 until height)) {
+            for (col in (0 until width)) {
+                yield(getTree(row, col))
+            }
+        }
     }
-    if (visibleFromTop) return true
-
-    return col.slice(cell.first + 1 until col.size).all {
-        it < cellValue
-    }
-}
-
-fun scenicScore(grid: List<List<Int>>, cell: Pair<Int, Int>): Int {
-    val cellValue = grid[cell.first][cell.second]
-    val indexedRow = grid[cell.first].mapIndexed { index, cell -> Pair(index, cell) }
-    val indexedCol = grid.map { it[cell.second] }. mapIndexed { index, cell -> Pair(index, cell) }
-
-    val topBlocker = (cell.first - 1 downTo 0).find {
-        indexedCol[it].second >= cellValue
-    } ?: 0
-
-    val topDistance = cell.first - topBlocker
-
-    val bottomBlocker = (cell.first + 1 until indexedCol.size).find {
-        indexedCol[it].second >= cellValue
-    } ?: indexedCol.size - 1
-
-    val bottomDistance = bottomBlocker - cell.first
-
-    val leftBlocker = (cell.second - 1 downTo 0).find {
-        indexedRow[it].second >= cellValue
-    } ?: 0
-    val leftDistance = cell.second - leftBlocker
-
-    val rightBlocker = (cell.second + 1 until indexedRow.size).find {
-        indexedRow[it].second >= cellValue
-    } ?: indexedRow.size - 1
-    val rightDistance = rightBlocker - cell.second
-
-    return topDistance * bottomDistance * leftDistance * rightDistance
 }
 
 
 fun main() {
     val input = File("input.txt").readLines()
-    val treeGrid = buildGrid(input)
+    val treeGrid = Grid(input)
 
-    val rows = treeGrid.size
-    val cols = treeGrid[0].size
-    val cells = (0 until rows).map { row ->
-        (0 until cols).map { col -> Pair(row, col)}
-    }.flatten()
-
-    // Part 1
-    val visibleTrees = cells.filter { isVisible(treeGrid, it).also{vis -> if (vis) println("$it.first, $it.second") } }
+    val visibleTrees = treeGrid.treeIterator().filter { it.isVisible() }.toList()
     println("Solution 1: ${visibleTrees.size}")
 
-    // Part 2
-    val maxScore = cells.map { scenicScore(treeGrid, it) }.max()
+    val maxScore = treeGrid.treeIterator().map { it.scenicScore() }.max()
     println("Solution 2: $maxScore")
 }
